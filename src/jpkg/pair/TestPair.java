cabbac742a3e15ae6437b2d9d3b2c31301e7002a
minerguy31 <minerguy31@gmail.com>
2016-09-25 20:30:57 -0600

    Add unit testing

---
+++ src/jpkg/pair/TestPair.java
@@ -0,0 +1,49 @@
+package jpkg.pair;
+
+import static org.junit.Assert.*;
+import jpkg.isarray.TestIsArray;
+import jpkg.test.TestManager;
+
+import org.junit.Test;
+
+public class TestPair {
+
+	public static void main(String[] args) {
+		test();
+		TestManager.runTests();
+	}
+	
+	public static void test() {
+		TestManager.addTest(TestPair.class);
+		TestIsArray.test();
+	}
+	
+	@Test
+	public void testBasic() {
+		Pair<String, Integer> p = new Pair<>("hi", 1);
+		
+		assertEquals(p.getLeft(), "hi");
+		assertEquals(p.getRight(), Integer.valueOf(1));
+	}
+	
+	@Test
+	public void testToString() {
+		Pair<String, Integer> p = new Pair<>("hi", 1);
+		Pair<String, Integer> nullpair = new Pair<>(null, null);
+		
+		assertEquals(p.toString(), "(hi, 1)");
+		assertEquals(nullpair.toString(), "(null, null)");
+	}
+	
+	@Test
+	public void testEquals() {
+		Pair<String, Integer> p1 = new Pair<>("hi", 1);
+		Pair<String, Integer> p2 = new Pair<>("hi", 1);
+		Pair<String, Integer> p3 = new Pair<>(null, null);
+
+		assertTrue(p1.equals(p2));
+		assertTrue(p2.equals(p1));
+		assertFalse(p2.equals(p3));
+		assertFalse(p3.equals(p2));
+	}
+}
